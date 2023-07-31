e0323abd961e6b5d2f2a8eca051d043689abb308
Ikram Maulana <ikram075@ummi.ac.id>
2023-08-01 06:50:27 +0700

    Feat: Configure Site Metadata

---
+++ src/lib/config.ts
@@ -0,0 +1,29 @@
+export const SITE = {
+  name: "Neon Blog",
+
+  origin: "https://neon-blog.vercel.app",
+  basePathname: "/",
+  trailingSlash: false,
+
+  title: "Neon Blog | Next.js and Notion Powered Blog",
+  description:
+    "A Next.js blog site that uses a new application router powered by the Notion Public API.",
+  applicationName: "Neon Blog",
+  keywords: [
+    "nextjs",
+    "notion",
+    "notion-api",
+    "notion-blog",
+    "nextjs13",
+    "app-router",
+    "nextjs-notion-blog",
+  ],
+  authors: [
+    {
+      name: "Ikram Maulana",
+      url: "https://ikram-maulana.tech",
+    },
+  ],
+  creator: "Ikram Maulana",
+  themeColor: "#F9F5EB",
+};
