81996070b90bc0a15eeb6066397e920b5ca6f4e9
Ikram Maulana <ikram075@ummi.ac.id>
2023-08-02 20:39:27 +0700

    Feat: Add Get Posts API

---
+++ src/helpers/posts/get-posts.ts
@@ -0,0 +1,25 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import { useInfiniteQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+const fetchData = async (page: string) => {
+  const { data } = await axios.get(`/api/posts?_page=${page ? page : ""}`, {
+    headers: {
+      "Cache-Control": "no-store",
+    },
+  });
+  return data.data;
+};
+
+export const getPosts = () => {
+  return useInfiniteQuery({
+    queryKey: ["posts"],
+    queryFn: async ({ pageParam = undefined }) => {
+      return await fetchData(pageParam);
+    },
+    getNextPageParam: (lastPage) => {
+      if (!lastPage.hasMore) return undefined;
+      return lastPage.nextCursor;
+    },
+  });
+};
