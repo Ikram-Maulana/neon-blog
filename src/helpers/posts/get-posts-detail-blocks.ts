d6aa94b1af8bbc67329a81b8148900269d67e90b
Ikram Maulana <ikram075@ummi.ac.id>
2023-08-03 17:43:11 +0700

    Feat: Update Get Posts Detail Blocks API and Request

--- src/helpers/posts/get-posts-detail-blocks.ts
+++ src/helpers/posts/get-posts-detail-blocks.ts
@@ -2,15 +2,18 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
-export const getPostsDetailBlocks = (id: string) => {
+export const getPostsDetailBlocks = (slug: string, id: string) => {
   return useQuery({
-    queryKey: ["posts-detail-blocks", id],
+    queryKey: ["posts-detail", slug, "blocks", id],
     queryFn: async () => {
-      const { data } = await axios.get(`/api/posts/${id}`, {
-        headers: {
-          "Cache-Control": "no-store",
-        },
-      });
+      const { data } = await axios.get(
+        `/api/posts/detail/${slug}/blocks/${id}`,
+        {
+          headers: {
+            "Cache-Control": "no-store",
+          },
+        }
+      );
       return data.data;
     },
   });
